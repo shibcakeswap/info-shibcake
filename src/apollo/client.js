2f280bd5320554a00590fb513b71e908cce80d0f
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-26 20:45:06 +0400

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,23 +1,15 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
-import { SupportedNetwork } from "../constants";
 
-[SupportedNetwork.BSC]: new ApolloClient({
-    link: new HttpLink({
-      uri: "https://thegraph.com/studio/subgraph/scssubgraph",
-    }),
-    cache: new InMemoryCache(),
-    shouldBatch: true,
-  }),
   
-// export const client = new ApolloClient({
-//  link: new HttpLink({
-//    uri: 'https://thegraph.com/studio/subgraph/scssubgraph',
-//  }),
-//  cache: new InMemoryCache(),
-//  shouldBatch: true,
-// })
+export const client = new ApolloClient({
+link: new HttpLink({
+uri: 'https://thegraph.com/studio/subgraph/scssubgraph',
+}),
+cache: new InMemoryCache(),
+shouldBatch: true,
+})
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
