f47d05f22bae40a88ae6c5ffe72eab23d3af5a77
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-27 12:04:49 +0400

    Update queries.js

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
 query health {
-    indexingStatusForCurrentVersion(subgraphName: "shibcakeswap/scs-subgraph") {
+    indexingStatusForCurrentVersion(subgraphName: "shibcakeswap/shibcakeswapsubgraphv1") {
       synced
       health
       chains {
